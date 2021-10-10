3d9a942b9a39999986bc6a77d403b1d28cb30245
mutiaraellwid <92252280+mutiaraellwid@users.noreply.github.com>
2021-10-10 20:06:40 +0700

    Update DataModel.php

--- app/Models/DataModel.php
+++ app/Models/DataModel.php
@@ -6,9 +6,17 @@ use CodeIgniter\Model;
 
 class DataModel extends Model
 {
-    protected $table      = 'databanjir';
-    protected $primaryKey = 'DateTime';
+    protected $table      = 'DataBanjir';
+    protected $primaryKey = 'ID';
     // protected $table      = 'DataBanjir';
     // protected $primaryKey = 'ID';
 
 }
+// class DataModel extends Model
+// {
+//     protected $table      = 'databanjir';
+//     protected $primaryKey = 'DateTime';
+//     // protected $table      = 'DataBanjir';
+//     // protected $primaryKey = 'ID';
+
+// }
