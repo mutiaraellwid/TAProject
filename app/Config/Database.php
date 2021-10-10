5099b14c90307bec83b70a9d35e0b3317f721402
mutiaraellwid <92252280+mutiaraellwid@users.noreply.github.com>
2021-10-10 20:02:28 +0700

    Update Database.php

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -32,10 +32,10 @@ class Database extends Config
 	 */
 	public $default = [
 		'DSN'      => '',
-		'hostname' => 'localhost',
-		'username' => 'root',
-		'password' => '',
-		'database' => 'test',
+		'hostname' => '103.168.139.48',
+		'username' => 'misred',
+		'password' => 'Misrednodesensor1@',
+		'database' => 'DB_Banjir',
 		'DBDriver' => 'MySQLi',
 		'DBPrefix' => '',
 		'pConnect' => false,
